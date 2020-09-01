ec13b88960d464aa7afd22fa650664380a70b8d7
U201716991 <dwawayu@163.com>
2020-09-01 23:37:08 +0800

    wry test commit

--- app/src/main/java/com/example/sns/MainActivity.java
+++ app/src/main/java/com/example/sns/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Test commit CZJ 2020.9.1.17.25
+        //I also want to have a test, please!!!! WRY 2020.9.1.23:35
     }
 }
