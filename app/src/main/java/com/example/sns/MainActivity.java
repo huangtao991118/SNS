b16b93db158a70b38b46813add43069ef46e4bbf
huangtao <wx_c22acb1167014b72a45948967a17bd88@git.code.tencent.com>
2020-09-02 16:34:30 +0800

    login_ui

--- app/src/main/java/com/example/sns/MainActivity.java
+++ app/src/main/java/com/example/sns/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.sns;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test commit CZJ 17.14
+        //Test commit CZJ 17：14
     }
 }
---
+++ app/src/main/java/com/example/sns/WelcomeActivity.java
@@ -0,0 +1,43 @@
+package com.example.sns;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Handler handler = new Handler();
+        handler.postDelayed(new MyRunnable(), 2000);
+    }
+
+
+    class MyRunnable implements Runnable{
+        @Override
+        public void run() {
+            Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+            startActivity(intent);
+            WelcomeActivity.this.finish();
+        }
+
+    }
+
+
+}
