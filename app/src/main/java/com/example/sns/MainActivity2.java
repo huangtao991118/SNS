313c4fab9c168d1ee53f693fe2edc9c9c870f72a
huangtao <wx_c22acb1167014b72a45948967a17bd88@git.code.tencent.com>
2020-09-03 08:27:00 +0800

    application_name

---
+++ app/src/main/java/com/example/sns/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.example.sns;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
