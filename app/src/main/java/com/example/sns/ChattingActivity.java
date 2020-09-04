5cee643724ac3df69845a804cf2a777b890f223b
huangtao <wx_c22acb1167014b72a45948967a17bd88@git.code.tencent.com>
2020-09-04 09:48:52 +0800

    11

--- app/src/main/java/com/example/sns/ChattingActivity.java
+++ app/src/main/java/com/example/sns/ChattingActivity.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity2 extends AppCompatActivity {
+public class ChattingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatting);
     }
 }
--- app/src/main/java/com/example/sns/MainActivity.java
+++ app/src/main/java/com/example/sns/MainActivity.java
@@ -2,16 +2,37 @@ package com.example.sns;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test commit CZJ 17：14
+        initView();
+    }
+
+    void initView(){
+        login = findViewById(R.id.login);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case (R.id.login):
+                Intent intent = new Intent(this,ChattingActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
